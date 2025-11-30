50b161996df322cf659a9386ff53b12eb72a044c
ReemasMoh <reemas.mohammed13@gmail.com>
2025-11-30 21:03:47 +0300

    Update SimpleEncryption.java

--- SimpleEncryption.java
+++ SimpleEncryption.java
@@ -4,44 +4,24 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class SimpleEncryption {
-
-    private static SecretKey generateKey() throws Exception {
+    public static void main(String[] args) throws Exception {
+        // Create a secret key
         KeyGenerator keyG = KeyGenerator.getInstance("AES");
         keyG.init(128);
-        return keyG.generateKey();
-    }
+        SecretKey secretKey = keyG.generateKey();
 
-    public static String encrypt(SecretKey key, String data) throws Exception {
-        Cipher encryptor = Cipher.getInstance("AES");
-        encryptor.init(Cipher.ENCRYPT_MODE, key);
-        byte[] enc = encryptor.doFinal(data.getBytes("UTF-8"));
-        return Base64.getEncoder().encodeToString(enc);
-    }
-
-    public static String decrypt(SecretKey key, String encryptedData) throws Exception {
-        Cipher encryptor = Cipher.getInstance("AES");
-        encryptor.init(Cipher.DECRYPT_MODE, key);
-        byte[] enc = Base64.getDecoder().decode(encryptedData);
-        return new String(encryptor.doFinal(enc), "UTF-8");
-    }
-    
-    public static void main(String[] args) throws Exception {
-        SecretKey secretKey = generateKey();
         String data = "UserCard=4111111111111111";
 
-        String encrypted = encrypt(secretKey, data);
-        System.out.println("Encrypted: " + encrypted);
+        // Encrypt
+        Cipher encryptor = Cipher.getInstance("AES");
+        encryptor.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] enc = encryptor.doFinal(data.getBytes());
+        System.out.println("Encrypted: " + Base64.getEncoder().encodeToString(enc));
 
-        String decrypted = decrypt(secretKey, encrypted);
-        System.out.println("Decrypted: " + decrypted);
+        // Decrypt
+        encryptor.init(Cipher.DECRYPT_MODE, secretKey);
+        String decryptedData = new String(encryptor.doFinal(enc));
+        System.out.println("Decrypted: " + decryptedData);
 
-        
-        // Non-Functional Test: Performance
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-        encrypt(secretKey, data);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("Performance Test (1000 encryptions): " + (end - start) + " ms");
     }
 }
